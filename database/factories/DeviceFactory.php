ba17093497bd16325331d70e0ad22af53cb29f5b
David <david@docampo.ch>
2020-12-17 16:48:34 +0100

    Added Files for Device Model

---
+++ database/factories/DeviceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Device;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DeviceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Device::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'last_update' => Carbon::now()->toDateTimeString(),
+        ];
+    }
+}
