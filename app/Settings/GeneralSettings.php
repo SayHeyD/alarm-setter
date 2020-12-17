5761d13fe4850aef4e4bf1bef86d9c8cd3fc589f
David <david@docampo.ch>
2020-12-17 10:49:30 +0100

    updated to php 7.4

---
+++ app/Settings/GeneralSettings.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Settings;
+
+
+use Spatie\LaravelSettings\Settings;
+
+class GeneralSettings extends Settings
+{
+    public float $topLimit;
+    public float $bottomLimit;
+
+    public static function group(): string
+    {
+        return 'general';
+    }
+}
