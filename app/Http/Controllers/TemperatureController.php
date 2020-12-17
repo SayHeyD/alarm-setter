4da6c5311be5c11991c2671fb00ef6042e211baa
David <david@docampo.ch>
2020-12-17 12:24:58 +0100

    removed empty line and added create method

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SetTempLimitsRequest;
 use App\Settings\GeneralSettings;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class TemperatureController extends Controller
 {
-    public function setLimits(SetTempLimitsRequest $request, GeneralSettings $settings)
+    public function setLimits(SetTempLimitsRequest $request, GeneralSettings $settings): RedirectResponse
     {
         $settings->topLimit = $request->topLimit;
         $settings->bottomLimit = $request->bottomLimit;
         $settings->save();
+
+        return redirect()->back()->with('success', 'Die Limits wurden gesetzt und werden in den nächsten 10 Sekunden aktualisiert');
     }
 
     public function create()
