9e8206956f4fac0bce7aeba3f38a9d66080955b0
David <david@docampo.ch>
2020-12-17 17:18:21 +0100

    added functionality to TemperatureController.php create method

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateTempEntryRequest;
 use App\Http\Requests\SetTempLimitsRequest;
 use App\Settings\GeneralSettings;
 use Illuminate\Http\RedirectResponse;
+use App\Models\Temperature;
+use App\Models\Device;
 
 class TemperatureController extends Controller
 {
@@ -17,8 +20,34 @@ class TemperatureController extends Controller
         return redirect()->back()->with('success', 'Die Limits wurden gesetzt und werden in den nächsten 10 Sekunden aktualisiert');
     }
 
-    public function create()
+    public function create(CreateTempEntryRequest $request, GeneralSettings $settings)
     {
+        $device = Device::where('device_id', $request->device);
 
+        $returnCode = 0;
+
+        if ($device == null)
+        {
+            $device = new Device();
+            $device->device_id = $request->device;
+        }
+
+        $device->recorded = $request->recorded;
+        $device->recorded_at = $request->recorded_at;
+
+        $device->save();
+
+        if ($device->recorded >= $settings->topLimit)
+        {
+            $returnCode = 1;
+        }
+        elseif ($device->recorded <= $settings->bottomLimit)
+        {
+            $returnCode = 2;
+        }
+
+        return response()->json([
+            'code' => $returnCode,
+        ]);
     }
 }
