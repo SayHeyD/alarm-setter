b6aae5e5a3e205ce10bb38490aeda7a8054da461
David <david@docampo.ch>
2020-12-17 10:36:21 +0100

    added values to DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\Temperature;
 
 class DashboardController extends Controller
 {
     public function view(): Response
     {
+
         return Inertia::render('Dashboard', [
-            'CurrentTemp' => null,
-            'topLimit' => null,
-            'bottomLimit' => null,
+            'CurrentTemp' => Temperature::latest('created_at')->first()->recorded,
+            'topLimit' => Temperature::latest('created_at')->first()->topLimit,
+            'bottomLimit' => Temperature::latest('created_at')->first()->bottomLimit,
         ]);
     }
 }
