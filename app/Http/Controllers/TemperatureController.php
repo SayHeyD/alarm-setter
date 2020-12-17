09810a8857ded9d081f8907e2b4a996f58d40b9c
David <david@docampo.ch>
2020-12-17 11:36:11 +0100

    created SetTempLimitsRequest.php and added it to TemperatureController.php

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SetTempLimitsRequest;
+use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
 
 class TemperatureController extends Controller
 {
-    public function setLimits()
+    public function setLimits(SetTempLimitsRequest $request, GeneralSettings $settings)
     {
-
+        $settings->topLimit = $request->topLimit;
+        $settings->bottomLimit = $request->bottomLimit;
+        $settings->save();
     }
 
     public function create()
---
+++ app/Http/Requests/SetTempLimitsRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class SetTempLimitsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'topLimit' => ['required', 'numeric', 'between:-5,60', 'gt:bottomLimit'],
+            'bottomLimit' => ['required', 'numeric', 'between:-5,60'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'topLimit.required' => 'Oberes Limit muss ausgefüllt sein!',
+            'topLimit.gt' => 'Oberes Limit muss höher sein als das Untere Limit',
+            'bottomLimit.required' => 'Unteres Limit muss ausgefüllt sein!',
+            'topLimit.between' => 'Das Limit muss zwischen -5 und 50 sein!',
+            'bottomLimit.between' => 'Das Limit muss zwischen -5 und 50 sein!',
+            'topLimit.numeric' => 'Oberes Limit muss ein numerischer Wert sein!',
+            'bottomLimit.numeric' => 'Unteres Limit muss ein numerischer Wert sein!',
+        ];
+    }
+}
